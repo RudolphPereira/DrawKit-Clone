73eb3a4fe14993b74a812fc91b557f8a11657956
Rudolph Pereira <rudolph.pereira93@gmail.com>
2020-10-02 17:48:32 +0530

    Updates

---
+++ precache-manifest.61e2253d617776dba651949ce2542c93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92262150f566f121503c01d5f68b0c32",
+    "url": "/Drawkit-Clone/index.html"
+  },
+  {
+    "revision": "d0a76f785daf3375a273",
+    "url": "/Drawkit-Clone/static/css/main.9f7007be.chunk.css"
+  },
+  {
+    "revision": "cea9a30dc306f9c3f39c",
+    "url": "/Drawkit-Clone/static/js/2.eff7dfff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Drawkit-Clone/static/js/2.eff7dfff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0a76f785daf3375a273",
+    "url": "/Drawkit-Clone/static/js/main.d338f3ee.chunk.js"
+  },
+  {
+    "revision": "71087aef7d13a273ae43",
+    "url": "/Drawkit-Clone/static/js/runtime-main.0b186c92.js"
+  }
+]);
