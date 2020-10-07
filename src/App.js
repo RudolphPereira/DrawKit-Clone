680dfe44004a66187dd83e8eb28cf5384781569f
Rudolph Pereira <rudolph.pereira93@gmail.com>
2020-10-07 20:06:03 +0530

    Added HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import CardsSection from "./components/CardsSection";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
@@ -9,28 +10,35 @@ import WebPage from "./components/WebPage";
 import HomeTwo from "./components/HomeTwo";
 import CardsSectionTwo from "./components/CardsSectionTwo";
 import SignUp from "./components/SignUp";
+import PopUp from "./components/PopUp";
 
 function App() {
   return (
     <Router>
-      <div className="app">
-        <Switch>
-          <Route path="/Drawkit-Clone">
-            <Nav />
-            <Home />
-            <CardsSection />
-            <WebPage />
-            <Footer />
-          </Route>
-          <Route path="/Free-Icons">
-            <Nav />
-            <HomeTwo />
-            <CardsSectionTwo />
-            <SignUp />
-            <Footer />
-          </Route>
-        </Switch>
-      </div>
+      <HashRouter>
+        <div className="app">
+          <Switch>
+            <Route path="/Drawkit-Clone">
+              <Nav />
+              <Home />
+              <CardsSection />
+              <WebPage />
+              <Footer />
+            </Route>
+            <Route path="/Free-Icons">
+              <Nav />
+              <HomeTwo />
+              <CardsSectionTwo />
+              <SignUp />
+              <Footer />
+            </Route>
+            <Route path="/Requests">
+              <Nav />
+              <PopUp />
+            </Route>
+          </Switch>
+        </div>
+      </HashRouter>
     </Router>
   );
 }
